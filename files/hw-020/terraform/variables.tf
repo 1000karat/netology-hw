ac6d1c84e881649b3cb8afd41348fdc03a484afa
1000karat <92o3o92562@gmail.com>
2026-06-11 19:44:28 +0300

    task 2

--- files/hw-020/terraform/variables.tf
+++ files/hw-020/terraform/variables.tf
@@ -33,3 +33,44 @@ variable "vms_ssh_root_key" {
   default     = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFmPTu4BZNXVHPmRdEZGwVAzqweyE5ZTP2iixRNM1OiZ admin@G14"
   description = "ssh-keygen -t ed25519"
 }
+
+#vars vm_web_
+variable "vm_web_family" {
+  type    = string
+  default = "ubuntu-2004-lts"
+}
+
+variable "vm_web_name" {
+  type    = string
+  default = "netology-develop-platform-web"
+}
+
+variable "vm_web_platform_id" {
+  type    = string
+  default = "standard-v3"
+}
+
+variable "vm_web_cores" {
+  type    = number
+  default = 2
+}
+
+variable "vm_web_memory" {
+  type    = number
+  default = 1
+}
+
+variable "vm_web_core_fraction" {
+  type    = number
+  default = 20
+}
+
+variable "vm_web_preemptible" {
+  type    = bool
+  default = true
+}
+
+variable "vm_web_nat" {
+  type    = bool
+  default = true
+}
