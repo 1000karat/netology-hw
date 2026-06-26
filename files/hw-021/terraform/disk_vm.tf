f05e8155e3c4244391b1cd8d802091f6d730cd67
1000karat <92o3o92562@gmail.com>
2026-06-26 20:21:39 +0300

    add task 2.1

---
+++ files/hw-021/terraform/disk_vm.tf
@@ -0,0 +1,21 @@
+variable "set_disk_vm" {
+  type = object({
+    name = string,
+    size = number,
+    type = string
+  })
+  default = {
+    name = "dosk-vm",
+    size = 1,
+    type = "network-hdd"
+  }
+}
+
+resource "yandex_compute_disk" "disk_vm" {
+  count = 3
+
+  name  = "${var.set_disk_vm.name}-${count.index + 1}"
+  size  = var.set_disk_vm.size
+  type  = var.set_disk_vm.type
+  zone = var.default_zone
+}
